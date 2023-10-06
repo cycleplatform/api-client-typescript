1e1ea9e2336a85534f16af9123f5037662ebe0d4
Alexander Mattoni <5110855+mattoni@users.noreply.github.com>
2023-10-06 11:32:25 -0700

    enforce module type in package json

--- vite.config.ts
+++ vite.config.ts
@@ -14,17 +14,5 @@ export default defineConfig({
       // the proper extensions will be added
       fileName: "index",
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      //   external: ["vue"],
-      //   output: {
-      //     // Provide global variables to use in the UMD build
-      //     // for externalized deps
-      //     globals: {
-      //       vue: "Vue",
-      //     },
-      //   },
-    },
   },
 });
